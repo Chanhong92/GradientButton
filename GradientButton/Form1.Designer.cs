db04471f596079914b5113618440c4212a03b7e2
jeyang <ych1273@gmail.com>
2021-04-20 02:36:15 +0900

    기본 gradient button 생성

--- GradientButton/Form1.Designer.cs
+++ GradientButton/Form1.Designer.cs
@@ -29,13 +29,117 @@ namespace GradientButton
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gradientBT1 = new GradientButton.GradientBT();
+            this.gradientBT2 = new GradientButton.GradientBT();
+            this.gradientBT3 = new GradientButton.GradientBT();
+            this.gradientBT4 = new GradientButton.GradientBT();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.panel1.Controls.Add(this.gradientBT4);
+            this.panel1.Controls.Add(this.gradientBT3);
+            this.panel1.Controls.Add(this.gradientBT2);
+            this.panel1.Controls.Add(this.gradientBT1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(154, 452);
+            this.panel1.TabIndex = 0;
+            // 
+            // gradientBT1
+            // 
+            this.gradientBT1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gradientBT1.FirstColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.gradientBT1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientBT1.ForeColor = System.Drawing.Color.White;
+            this.gradientBT1.Location = new System.Drawing.Point(0, 0);
+            this.gradientBT1.Name = "gradientBT1";
+            this.gradientBT1.RotateAngle = 45F;
+            this.gradientBT1.SecondColor = System.Drawing.Color.DeepSkyBlue;
+            this.gradientBT1.Selected = true;
+            this.gradientBT1.Size = new System.Drawing.Size(154, 45);
+            this.gradientBT1.SplitLine = 0.5F;
+            this.gradientBT1.TabIndex = 1;
+            this.gradientBT1.Text = "gradientBT1";
+            this.gradientBT1.UseVisualStyleBackColor = true;
+            // 
+            // gradientBT2
+            // 
+            this.gradientBT2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gradientBT2.FirstColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.gradientBT2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientBT2.ForeColor = System.Drawing.Color.White;
+            this.gradientBT2.Location = new System.Drawing.Point(0, 45);
+            this.gradientBT2.Name = "gradientBT2";
+            this.gradientBT2.RotateAngle = 45F;
+            this.gradientBT2.SecondColor = System.Drawing.Color.DeepSkyBlue;
+            this.gradientBT2.Selected = false;
+            this.gradientBT2.Size = new System.Drawing.Size(154, 45);
+            this.gradientBT2.SplitLine = 0.5F;
+            this.gradientBT2.TabIndex = 2;
+            this.gradientBT2.Text = "gradientBT2";
+            this.gradientBT2.UseVisualStyleBackColor = true;
+            // 
+            // gradientBT3
+            // 
+            this.gradientBT3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gradientBT3.FirstColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.gradientBT3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientBT3.ForeColor = System.Drawing.Color.White;
+            this.gradientBT3.Location = new System.Drawing.Point(0, 90);
+            this.gradientBT3.Name = "gradientBT3";
+            this.gradientBT3.RotateAngle = 45F;
+            this.gradientBT3.SecondColor = System.Drawing.Color.DeepSkyBlue;
+            this.gradientBT3.Selected = false;
+            this.gradientBT3.Size = new System.Drawing.Size(154, 45);
+            this.gradientBT3.SplitLine = 0.5F;
+            this.gradientBT3.TabIndex = 3;
+            this.gradientBT3.Text = "gradientBT3";
+            this.gradientBT3.UseVisualStyleBackColor = true;
+            // 
+            // gradientBT4
+            // 
+            this.gradientBT4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gradientBT4.FirstColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.gradientBT4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.gradientBT4.ForeColor = System.Drawing.Color.White;
+            this.gradientBT4.Location = new System.Drawing.Point(0, 135);
+            this.gradientBT4.Name = "gradientBT4";
+            this.gradientBT4.RotateAngle = 45F;
+            this.gradientBT4.SecondColor = System.Drawing.Color.DeepSkyBlue;
+            this.gradientBT4.Selected = false;
+            this.gradientBT4.Size = new System.Drawing.Size(154, 45);
+            this.gradientBT4.SplitLine = 0.5F;
+            this.gradientBT4.TabIndex = 4;
+            this.gradientBT4.Text = "gradientBT4";
+            this.gradientBT4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(748, 452);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private GradientBT gradientBT1;
+        private GradientBT gradientBT4;
+        private GradientBT gradientBT3;
+        private GradientBT gradientBT2;
     }
 }
 
---
+++ GradientButton/GradientBT.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GradientButton
+{
+    class GradientBT:Button
+    {
+        private Color firstColor = Color.Black;
+        private Color secondColor = Color.SkyBlue;
+        private float splitLine = 0.5f;
+        private float rotate = 0f;
+        private bool selected = false;
+
+        public Color FirstColor
+        {
+            get
+            {
+                return firstColor;
+            }
+            set
+            {
+                firstColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color SecondColor
+        {
+            get
+            {
+                return secondColor;
+            }
+            set
+            {
+                secondColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public float RotateAngle
+        {
+            get
+            {
+                return rotate;
+            }
+            set
+            {
+                rotate = value;
+                this.Invalidate();
+            }
+        }
+
+        public float SplitLine
+        {
+            get
+            {
+                return splitLine;
+            }
+            set
+            {
+                splitLine = value;
+                this.Invalidate();
+            }
+        }
+
+        public bool Selected
+        {
+            get
+            {
+                return selected;
+            }
+            set
+            {
+                selected = value;
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            LinearGradientBrush lgb = new LinearGradientBrush(
+                this.ClientRectangle, firstColor, secondColor, 0f);
+
+            ColorBlend cb = new ColorBlend();
+            cb.Positions = new float[] { 0, splitLine, 1 };
+
+            if (selected == true) { cb.Colors = new Color[] { firstColor, firstColor, secondColor }; }
+            else { cb.Colors = new Color[] { firstColor, firstColor, firstColor }; }
+            
+            lgb.InterpolationColors = cb;
+
+            lgb.RotateTransform(rotate);
+
+            pevent.Graphics.FillRectangle(lgb, this.ClientRectangle);
+            lgb.Dispose();
+            SizeF textSize = pevent.Graphics.MeasureString(this.Text, this.Font);
+            pevent.Graphics.DrawString(this.Text, this.Font, new SolidBrush(this.ForeColor), 
+                (Width - textSize.Width) / 2, (Height - textSize.Height) / 2);
+        }
+    }
+}
